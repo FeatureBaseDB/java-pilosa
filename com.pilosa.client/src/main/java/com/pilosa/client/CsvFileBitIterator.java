3cc4aa50b30df24932d2274073cb95cae1ea1f8b
Yuce Tekol <yucetekol@gmail.com>
2017-03-05 04:24:11 +0300

    Added fromStream method to CsvFileBitIterator

--- com.pilosa.client/src/main/java/com/pilosa/client/CsvFileBitIterator.java
+++ com.pilosa.client/src/main/java/com/pilosa/client/CsvFileBitIterator.java
@@ -4,8 +4,17 @@ import com.pilosa.client.internal.ClientProtos;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
 
+/**
+ * Iterates over a CSV of bitmaps.
+ * <p>
+ * The CSV file should not have a header and should have the following structure:
+ * <pre>
+ *     BITMAP_ID,PROFILE_ID[,TIMESTAMP]
+ * </pre>
+ */
 public class CsvFileBitIterator implements IBitIterator {
     private Scanner scanner = null;
     private ClientProtos.Bit nextBit = null;
@@ -13,9 +22,26 @@ public class CsvFileBitIterator implements IBitIterator {
     private CsvFileBitIterator() {
     }
 
+    /**
+     * Creates a bit iterator from the CSV file at the given path.
+     * @param path of the CSV file
+     * @return bit iterator
+     * @throws FileNotFoundException
+     */
     public static CsvFileBitIterator fromPath(String path) throws FileNotFoundException {
+        return fromStream(new FileInputStream(path));
+    }
+
+    /**
+     * Creates a bit iterator from an input stream.
+     *
+     * @param stream CSV stream
+     * @return bit iterator
+     * @throws FileNotFoundException
+     */
+    public static CsvFileBitIterator fromStream(InputStream stream) throws FileNotFoundException {
         CsvFileBitIterator iterator = new CsvFileBitIterator();
-        iterator.scanner = new Scanner(new FileInputStream(path));
+        iterator.scanner = new Scanner(stream);
         return iterator;
     }
 

7bc8cc5b644cca8bbec4d07d39cb9dcc4f5e439d
Yuce Tekol <yucetekol@gmail.com>
2018-06-27 13:21:24 +0300

    Send protobuf headers when needed

--- com.pilosa.client/src/main/java/com/pilosa/client/PilosaClient.java
+++ com.pilosa.client/src/main/java/com/pilosa/client/PilosaClient.java
@@ -196,7 +196,7 @@ public class PilosaClient implements AutoCloseable {
         String path = String.format("/index/%s", index.getName());
         String body = "";
         ByteArrayEntity data = new ByteArrayEntity(body.getBytes(StandardCharsets.UTF_8));
-        clientExecute("POST", path, data, protobufHeaders, "Error while creating index");
+        clientExecute("POST", path, data, null, "Error while creating index");
     }
 
     /**
@@ -223,7 +223,7 @@ public class PilosaClient implements AutoCloseable {
         String path = String.format("/index/%s/field/%s", field.getIndex().getName(), field.getName());
         String body = field.getOptions().toString();
         ByteArrayEntity data = new ByteArrayEntity(body.getBytes(StandardCharsets.UTF_8));
-        clientExecute("POST", path, data, protobufHeaders, "Error while creating field");
+        clientExecute("POST", path, data, null, "Error while creating field");
     }
 
     /**
@@ -549,7 +549,9 @@ public class PilosaClient implements AutoCloseable {
             default:
                 throw new IllegalArgumentException(String.format("%s is not a valid HTTP method", method));
         }
-        request.setHeaders(headers);
+        if (headers != null) {
+            request.setHeaders(headers);
+        }
         return request;
     }
 
